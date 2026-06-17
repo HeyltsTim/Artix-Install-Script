cf2525b2931c175d221a10b8d626431eb04d2fc7
HeyltsTim <112197473+HeyltsTim@users.noreply.github.com>
2026-06-17 09:54:47 -0400

    Refactor post-install.sh to use config files
    Read package and service names from configuration files and install them.

--- post-install.sh
+++ post-install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+mapfile -t packages < <(grep -vE '^\s*#|^\s*$'./packages.conf)
+mapfile -t services < <(grep -vE '^\s*#|^\s*$'./services.conf)
+
 enable_srv() {
 SERVICE=${1}
 dinitctl enable ${SERVICE}
 dinitctl start ${SERVICE}
 }
-enable_srv turnstiled
 
-pacman -S firewalld-dinit pipewire-dinit
+pacman -S ${packages[@]}
+
+for srv in ${services[@]}
+do
+enable_srv ${srv}
+done
+
 
