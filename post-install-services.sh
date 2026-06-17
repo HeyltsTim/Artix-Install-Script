fd3719779754db70625903b24cbd4028808d270d
HeyltsTim <112197473+HeyltsTim@users.noreply.github.com>
2026-06-17 09:57:38 -0400

    Rename post-install.sh to post-install-services.sh

--- post-install-services.sh
+++ post-install-services.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-mapfile -t packages < <(grep -vE '^\s*#|^\s*$'./packages.conf)
 mapfile -t services < <(grep -vE '^\s*#|^\s*$'./services.conf)
 
 enable_srv() {
@@ -8,8 +7,6 @@ SERVICE=${1}
 dinitctl enable ${SERVICE}
 dinitctl start ${SERVICE}
 }
-
-pacman -S ${packages[@]}
 
 for srv in ${services[@]}
 do
