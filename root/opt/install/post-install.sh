1187647b0b915657fc9c796a49a182e701824521
HeyltsTim <112197473+HeyltsTim@users.noreply.github.com>
2026-06-22 17:48:13 -0400

    Initialize and enable services in post-install script
    Added initialization for enabling services using dinitctl.

--- root/opt/install/post-install.sh
+++ root/opt/install/post-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-
+#init
+SERVICES=("syslog-ng" "lxd" "chrony" "dbus" "acpid" "chrony" "firewalld" "sshd" "turnstiled" "elogind" "lm_sensors" "dnsmasq" "zramen")
+for i in {${SERVICES[@]}}; do
+dinitctl enable $i
+done
 # end of script
 rm -rf /opt/install /etc/dinit.d/boot.d/postinstall
