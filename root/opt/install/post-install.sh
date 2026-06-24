821f8d2979e8932b67319e29833ef6694bee05ad
HeyltsTim <112197473+HeyltsTim@users.noreply.github.com>
2026-06-24 11:09:56 -0400

    Remove btrfs subvolume creation for cachyos
    Removed the creation of the btrfs subvolume for cachyos.

--- root/opt/install/post-install.sh
+++ root/opt/install/post-install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-btrfs subvolume create /var/lib/lxd/storage-pools/cachyos
-
 #init
 SERVICES=("syslog-ng" "lxd" "chrony" "dbus" "acpid" "chrony" "firewalld" "sshd" "turnstiled" "elogind" "lm_sensors" "dnsmasq" "fail2ban" "dhcpcd" "apparmor" "zramen")
 for i in "${SERVICES[@]}"; do
